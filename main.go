ef539cbb9b002222eac37d0af7004f70d59a5b16
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2024-03-07 17:46:27 +0700

    update auth

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"auth/internal/adapters/handler"
 	"auth/internal/adapters/repo"
 	"auth/internal/core/domain"
 	"auth/internal/core/services"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -14,7 +17,9 @@ var (
 )
 
 func main() {
-
+	router := fiber.New()
+	router.Use(cors.New())
+	
 	// Connection to PostgreSQL
 	dsn := "user=postgres password=testpass123 dbname=go-hex port=3500 sslmode=disable TimeZone=Asia/Bangkok"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -27,4 +32,11 @@ func main() {
 	store := repo.NewDB(db)
 
 	authService = services.NewAuthService(store)
+
+	v1 := router.Group("/api")
+
+	authHandler := handler.NewAuthHandler(*authService)
+	v1.Post("/signup", authHandler.SignUp)
+
+	router.Listen(":8080")
 }
