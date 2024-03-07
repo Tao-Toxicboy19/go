40b67c9d99dec50076546a1bc7ec9dc7af4bd135
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2024-03-07 17:08:25 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// Connection to PostgreSQL
+	dsn := "user=postgres password=testpass123 dbname=go-hex port=3500 sslmode=disable TimeZone=Asia/Bangkok"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+}
