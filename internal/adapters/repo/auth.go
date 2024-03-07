af1e295e4135ca975c38981f982a18bd5ae1e362
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2024-03-07 17:26:02 +0700

    update auth

--- internal/adapters/repo/auth.go
+++ internal/adapters/repo/auth.go
@@ -1,4 +1,23 @@
 package repo
 
+func (a *DB) SignUp(user *domain.User) (*domain.User, error) {
+	req := a.db.Where("username = ? ", user.Username).First(&user)
 
+	if req.RowsAffected != 0 {
+		return nil, errors.New("user already exists")
+	}
 
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 14)
+	if err != nil {
+		return nil, fmt.Errorf("password not hashed: %v", err)
+	}
+
+	user.ID = uuid.New().String()
+	user.Password = string(hash)
+
+	req = a.db.Create(&user)
+	if req.RowsAffected == 0 {
+		return nil, fmt.Errorf("user not saved: %v", req.Error)
+	}
+	return user, nil
+}
